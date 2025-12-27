7dd853808da1e45f3b477ecd2b65e0b742c310c8
harvzor <harveyjwilliams@aol.co.uk>
2025-12-27 15:47:36 +0100

    Added Circle and made the Critter circular.

---
+++ Misc/Circle.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public partial class Circle : Node2D
+{
+    [Export] public float Radius = 32;
+    [Export] public Color Color = Colors.White;
+
+    public override void _Draw()
+    {
+        this.DrawCircle(new Vector2(0, 0), Radius, new Color(Color));
+    }
+}
