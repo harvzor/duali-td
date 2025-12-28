84e3be91c4a72c793e4a21699610bb966b6ed07c
harvzor <harveyjwilliams@aol.co.uk>
2025-12-28 03:05:57 +0100

    Only allow building on certain areas.

--- UserInterface/TowerPlacement.cs
+++ UserInterface/TowerPlacement.cs
@@ -1,6 +1,21 @@
 public partial class TowerPlacement : Panel
 {
 	private UserInterface _userInterface;
+
+	private bool IsBuildable()
+	{
+		TileMapLayer buildable = (this
+			.GetTree()
+			.GetRoot()
+			.GetChild(0)
+			.FindChild(nameof(Map))!
+			.FindChild("Buildable") as TileMapLayer)!;
+
+		Vector2 mouseLocal = buildable.ToLocal(GetGlobalMousePosition());
+		Vector2I cell = buildable.LocalToMap(mouseLocal);
+
+		return buildable.GetCellSourceId(cell) != -1;
+	}
 	
 	public override void _Ready()
 	{
@@ -12,6 +27,12 @@ public partial class TowerPlacement : Panel
 		// On hover
 		if (inputEvent is InputEventMouseMotion inputEventMouseMotion)
 		{
+			if (!IsBuildable())
+			{
+				_userInterface.HideTower();
+				return;
+			}
+
 			_userInterface.ShowTower(inputEventMouseMotion.Position);
 			return;
 		}
@@ -19,6 +40,9 @@ public partial class TowerPlacement : Panel
 		// On left click release
 		if (inputEvent is InputEventMouseButton { ButtonIndex: MouseButton.Left, ButtonMask: 0 } inputEventMouseButton)
 		{
+			if (!IsBuildable())
+				return;
+
 			_userInterface.TrySpawnTower(inputEventMouseButton.Position);
 		}
 	}
